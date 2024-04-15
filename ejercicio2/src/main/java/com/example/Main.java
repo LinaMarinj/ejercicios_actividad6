e12465c7265ea1e9eb9162f664d58a4e39c25576
LinaMarinj <linamarin2011@gmail.com>
2024-04-15 17:49:33 -0500

    se crean carpetas con los ejercicios

---
+++ ejercicio2/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        adivinarNumero(1, 100, 5);
+    }
+
+    // Método para calcular las deducciones
+    public static void adivinarNumero(int min_numero, int max_numero, int max_intentos) {
+       //Implementar solución
+    }
+}
